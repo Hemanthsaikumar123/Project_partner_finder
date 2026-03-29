dc124e4fe71f885451ae52e49ddc1da28fa68747
koppuhemanthsaikumar <koppuhemanthsaikumar123@gmail.com>
2026-03-29 23:50:43 +0530

    Error message updated

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -19,7 +19,7 @@ const authMiddleware = (req, res, next) => {
     next()
 
   } catch (error) {
-    return res.status(401).json({ message: "Invalid token" })
+    return res.status(401).json({ message: "Invalid token check again" })
   }
 
 }
